ce6a59e0401ac82032d26dfb0a788b2c6170acb6
SakshiJethwa2024 <sakshijethwa4@gmail.com>
2023-10-17 16:23:14 +0530

    sakshi's changes on 17th oct

--- server/models/user.js
+++ server/models/user.js
@@ -1,22 +1,37 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username:{
-    type:String,
-    required:'This field is required'
+  name: {
+    type: String,
+    required: 'This field is required.'
   },
-  email:{
-    type:String,
-    required:'This field is required'
-    },
-    password:{
-        type:String,
-        required:'This field is required'
-    },
-    role:{
-        type:String,
-    required:'This field is required'
+  email: {
+    type: String,
+    required: 'This field is required.',
+    unique: true,
+    validate: {
+      validator: emailValidate,
+      message: props => `${props.value} must have @ sign,domain must be one or more lowercase letters, numbers, underscores, dots, or hyphens.. and then another (escaped) dot, with the extension being 2 to 63 letters or dots`
+    }
   },
+  password: {
+    type: String,
+    required: 'This field is required.'
+  }
 });
 
-module.exports = mongoose.model('User',userSchema);
+userSchema.index({ name: 'text', description: 'text' });
+// WildCard Indexing
+//recipeSchema.index({ "$**" : 'text' });
+
+module.exports = mongoose.model('Userdata', userSchema);
+
+
+function emailValidate(value) {
+  var reg_email = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,63})$/;
+
+  if (!reg_email.test(value)) {
+    return false;
+  }
+  return true
+}
